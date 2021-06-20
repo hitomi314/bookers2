f8c920d7edeb08d54adda912552783bf496fbf16
EC2 Default User <boc.jrock@gmail.com>
2021-06-20 09:23:38 +0000

    [Add] Userinfo.image.edit.update[finish]4-5chapter

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '20fdcf64e2e476c2f60a2d04cdbb00f8c499a4e35d5ff68361a4eb2771cd20beb88ca4243ef62f9c08a134582361ff723de9ae31157db5613b567286f25d92a4'
