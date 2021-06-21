c4f378eee1afe7c19f0e980cebe4d99d69afc050
EC2 Default User <boc.jrock@gmail.com>
2021-06-21 14:20:50 +0000

    [Add]flashmessege

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,7 +22,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
      if @book.update(book_params)
-      flash[:succes] = 'Book was successfully updated.'
+      flash[:succes] = 'You have updated book successfully.'
       redirect_to book_path(@book)
      else
       render :edit
@@ -33,7 +33,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      flash[:succes] = 'Book was successfully created.'
+      flash[:succes] = 'You have created book successfully.'
       redirect_to book_path(@book)
     else
       @books = Book.all
