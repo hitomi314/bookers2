1e2794baae7f7fb7c554c256f79ceb8f7ba34369
EC2 Default User <boc.jrock@gmail.com>
2021-07-02 13:27:07 +0000

    [Add]Favoritemodel

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "home/about" => "homes#about"
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update ,:index]
 
 end
