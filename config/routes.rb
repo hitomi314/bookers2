716c5ebf4e0795fcf5bca5e161b76e1abf969c49
EC2 Default User <boc.jrock@gmail.com>
2021-07-04 14:37:33 +0000

    [Finish]phase2-4

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,13 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:new, :create, :index, :show, :destroy]
   end
-  resources :users, only: [:show, :edit, :update ,:index]
+  resources :users, only: [:show, :edit, :update ,:index] do
+    # ——————————————— ここから ———————————————
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+    # ——————————— ここまでネストさせる ———————————
+  end
+  
 
 end
