7b0a913dd9b6ab381bac646ce47cd6c8e77024e4
EC2 Default User <boc.jrock@gmail.com>
2021-07-05 03:24:47 +0000

    [Add]surches

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "home/about" => "homes#about"
+  
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:new, :create, :index, :show, :destroy]
   end
+  
   resources :users, only: [:show, :edit, :update ,:index] do
     # ——————————————— ここから ———————————————
     resource :relationships, only: [:create, :destroy]
@@ -13,6 +15,8 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
     # ——————————— ここまでネストさせる ———————————
   end
+  
+  get '/search', to: 'searchs#search'
   
 
 end
